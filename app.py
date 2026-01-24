29a1f6a182f8ed7fe0d81bf7b6af221304ee0dd3
Paulo Correia <pjmcorreia72@gmail.com>
2026-01-24 20:38:47 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
 
 
-st.title('Flix App')
+def main():
+    st.title('Flix App')
+
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Início')
+
+    if menu_option == 'Gêneros':
+        st.write('Gêneros')
+
+    if menu_option == 'Atores/Atrizes':
+        st.write('Atores/Atrizes')
+    
+    if menu_option == 'Filmes':
+        st.write('Filmes')
+
+    if menu_option == 'Avaliações':
+        st.write('Avaliações')
+
+if __name__ == '__main__':
+    main()
